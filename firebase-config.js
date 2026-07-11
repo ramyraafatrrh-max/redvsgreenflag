1b6085786e5cdbc4bbde6f125ac1506a1fa5de7b
ramyraafatrrh-max <ramyraafat.rrh@gmail.com>
2026-07-11 16:54:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase configuration for Red Flag Green Flag Game.
+// This file should ONLY export firebaseConfig.
+// Do not import initializeApp here.
+// Do not initialize Firebase here.
+// app.js already handles Firebase initialization.
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyCtcM0xVhMdpdNGdsuS80X7futXslgCei0",
   authDomain: "redvsgreenflag-2e8ea.firebaseapp.com",
-  databaseURL: "https://redvsgreenflag-2e8ea-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://redvsgreenflag-2e8ea-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "redvsgreenflag-2e8ea",
   storageBucket: "redvsgreenflag-2e8ea.firebasestorage.app",
   messagingSenderId: "159874679159",
   appId: "1:159874679159:web:27bbd80cc68ab5e2be2172",
   measurementId: "G-M3WQ5D32EH"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
